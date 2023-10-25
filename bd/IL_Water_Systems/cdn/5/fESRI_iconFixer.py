fcd88bca91c6a293f7185eaeabe687206fc84873
Dan Newcomb <dan111174@gmail.com>
2023-10-25 08:57:43 -0500

    update to Water system BD app

---
+++ bd/IL_Water_Systems/cdn/5/fESRI_iconFixer.py
@@ -0,0 +1,40 @@
+#
+# Fix a Stupid Fucking ESRI bug in Experience Builder Exports
+#
+# Basically a bunch of widget icons get absolute links in the exports which are trying to pull images from localhost:3001
+# This must be fixed by making them relative links in the config.json file
+# 
+# Place this script in the main exported directory and run it to fix those links.
+#  ** This directory is something like cdn/1 - wherever the config.json file is **
+#
+# fESRI
+#
+import os
+
+# Get the current directory (the directory where your script is located)
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Change the working directory to the current directory
+os.chdir(current_directory)
+
+# Now, the working directory is set to the directory where your script is located
+
+fileToFix = "config.json"
+
+search_text1 = "https://localhost:3001/experience/../"
+search_text2 = "https://localhost:3001/builder/../"
+
+replacement_text = ""
+
+# Opening text file in read only mode
+with open(fileToFix, 'r') as file:
+	data = file.read()
+	data = data.replace(search_text1, replacement_text)
+	data = data.replace(search_text2, replacement_text)
+
+# Opening text file in write only mode
+with open(fileToFix, 'w') as file:
+	file.write(data)
+
+
+print("Text replaced")

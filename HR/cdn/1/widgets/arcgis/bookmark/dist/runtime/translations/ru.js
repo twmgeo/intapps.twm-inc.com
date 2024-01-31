8cd1b93fa97d30d253adfac0cf8e3b040e430c61
Dan Newcomb <dan111174@gmail.com>
2024-01-31 14:57:20 -0600

    push fresh build of HR app

--- HR/cdn/1/widgets/arcgis/bookmark/dist/runtime/translations/ru.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e){return{execute:function(){e({_widgetLabel:"Закладка",_widgetDescription:"Виджет определяет заданные географические местоположения и сохраняет их, чтобы обратиться к ним позднее.",addBookmark:"Добавить закладку",layoutTips:"Это пользовательская область",bookmarkList:"Список закладок",graphicLayer:"Закладка слоя графики"})}}}));
